d6ee38942beb10243976f44fb224f8435166ee56
Jackson Hayward <jchayward1@gmail.com>
2022-11-06 12:20:36 -0800

    Add written text

---
+++ scripts/TypeText.js
@@ -0,0 +1,25 @@
+function type(initial, speed, inuptElement, outputElement) {
+    input = inuptElement;
+    document.getElementById(outputElement).innerHTML += input.charAt(initial);
+    setTimeout(function(){
+        ((initial < input.length - 1) ? type(initial+1, speed, inuptElement, outputElement) : false);
+    }, speed);
+}
+
+
+const speed = 50;
+
+type(0, speed, "> Welcome to my website!;", "greeting");
+
+
+setTimeout(() => {
+    type(0,speed, "> My name is Jackson Hayward; ", "introduction");
+  }, 3000);
+
+  setTimeout(() => {
+    type(0,speed, "> I'm an aspiring software engineer; ", "about");
+  }, 6300);
+
+  setTimeout(() => {
+    type(0,speed, "> Scroll down to learn more about me; ", "closing");
+  }, 9000);
