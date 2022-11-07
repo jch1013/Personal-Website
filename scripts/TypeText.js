0941dbd16fdd727e54d5671a1f5e271620e2bf0e
Jackson Hayward <jchayward1@gmail.com>
2022-11-06 19:36:14 -0800

    Styling and new sections

--- scripts/TypeText.js
+++ scripts/TypeText.js
@@ -11,15 +11,15 @@ function type(initial, speed, inuptElement, outputElement) {
 
 
 /* Displaying lines, with delay between each line */
-const speed = 50;
+const speed = 30;
 
 type(0, speed, "> Welcome to my website! ;", "greeting");
 
 setTimeout(() => {
-    type(0,speed, "> My name is Jackson Hayward ; ", "introduction");}, 3000);
+    type(0,speed, "> My name is Jackson Hayward ; ", "introduction");}, 2000);
 
 setTimeout(() => {
-    type(0,speed, "> I'm an aspiring software engineer ; ", "about");}, 6300);
+    type(0,speed, "> I'm an aspiring software engineer ; ", "about");}, 4300);
 
 setTimeout(() => {
-    type(0,speed, "> Scroll down to learn more about me ; ", "closing");}, 9000);
+    type(0,speed, "> Scroll down to learn more about me ; ", "closing");}, 6500);
